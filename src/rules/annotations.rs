ee681894beec89354166d92eaca823e8354a4767
Emerson MX <emersonmx@gmail.com>
2025-06-13 07:49:30 -0300

    test: add trim whitespaces to annotations

--- src/rules/annotations.rs
+++ src/rules/annotations.rs
@@ -41,3 +41,25 @@ fn apply_annotation_rules(node: Node, source: &str, indent_level: usize) -> Stri
 
     output
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::format_code;
+    use rstest::*;
+
+    #[rstest]
+    #[case("@export var a = 0", "@export var a = 0\n")]
+    #[case("@export @onready var a = 0", "@export @onready var a = 0\n")]
+    #[case("  @export  var a = 0", "@export var a = 0\n")]
+    #[case("  @onready  @export  var a = 0", "@onready @export var a = 0\n")]
+    #[case("  @  onready  @  export  var a = 0", "@onready @export var a = 0\n")]
+    fn trim_whitespaces(#[case] source_input: &str, #[case] expected_output: &str) {
+        let formatted = format_code(source_input).unwrap();
+
+        assert_eq!(
+            formatted, expected_output,
+            "Failed for input: {:?}",
+            source_input
+        );
+    }
+}
