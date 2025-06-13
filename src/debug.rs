9ab52efbe3f9a8ce8e7613c5f9b05fc1552729d2
Emerson MX <emersonmx@gmail.com>
2025-06-13 07:42:01 -0300

    refactor: use output push and push_str

--- src/debug.rs
+++ src/debug.rs
@@ -2,18 +2,22 @@ use tree_sitter::Node;
 
 #[allow(dead_code)]
 pub fn node_to_string(node: Node, source: &str, indent_level: usize) -> String {
-    let mut output = String::new();
     let indent = "    ".repeat(indent_level);
     let text = &source[node.byte_range()];
     let kind = node.kind();
-
     let content_display = if !text.contains('\n') {
         format!(" `{}`", text.trim())
     } else {
         String::new()
     };
 
-    output.push_str(&format!("{}({}{})", indent, kind, content_display));
+    let mut output = String::new();
+
+    output.push_str(&indent);
+    output.push('(');
+    output.push_str(kind);
+    output.push_str(&content_display);
+    output.push(')');
     output.push('\n');
 
     for child in node.children(&mut node.walk()) {
