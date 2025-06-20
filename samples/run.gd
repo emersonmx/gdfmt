693522686bca6f846d3a0e5df69c224b6b3af2ac
Emerson MX <emersonmx@gmail.com>
2025-06-20 10:29:46 -0300

    chore: update run.gd sample

--- samples/run.gd
+++ samples/run.gd
@@ -1,1 +1,23 @@
-var a = 42
+# "hello world"
+var a = "hello world"
+
+# "hello world"
+var b = 'hello world'
+
+# 'hello" "world'
+var c = 'hello" "world'
+
+# "hello' 'world"
+var d = 'hello\' \'world'
+
+# 'hello" "world'
+var e = "hello\" \"world"
+
+# "hello\"' '\"world"
+var f = 'hello\"\' \'\"world'
+
+# "hello\"' '\"world"
+var g = 'hello\"\'\"\' \'\"\'\"world'
+
+# "hello\"'\"' '\"'\"world"
+var h = "hello\"\'\"\' \'\"\'\"world"
