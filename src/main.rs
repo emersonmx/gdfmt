61d5ae121a04233ab69e82f27954aa1741b9d078
Emerson MX <emersonmx@gmail.com>
2025-06-24 11:48:55 -0300

    feat: save formatted file when format from file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::{Context, Result};
 use gdfmt::format_code;
-use std::{fs::read_to_string, io::Read};
+use std::{
+    fs::{read_to_string, write},
+    io::Read,
+};
 
 fn main() -> Result<()> {
     let mut source = String::new();
@@ -10,14 +13,16 @@ fn main() -> Result<()> {
         let data = read_to_string(file_path)
             .with_context(|| format!("Unable to read from file: {}", file_path))?;
         source.push_str(&data);
+        let output = format_code(&source)?;
+        write(file_path, output)
+            .with_context(|| format!("Unable to write to file: {}", file_path))?;
     } else {
         std::io::stdin()
             .read_to_string(&mut source)
             .context("Unable to read from stdin.")?;
+        let output = format_code(&source)?;
+        print!("{}", output);
     }
-
-    let output = format_code(&source)?;
-    print!("{}", output);
 
     Ok(())
 }
